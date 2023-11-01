b326e61e86e344a9976e020726427f02927a1189
Mohammed Ashiqur Rahman Baig <ashiqur1107@gmail.com>
2023-11-01 20:42:22 +0600

    init project

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import { FC, useState } from "react";
+
+const App: FC = () => {
+  const [galleryItems , setGalleryItems] = useState()
+
+  return (
+    <>
+      <div className="w-full h-auto min-h-screen bg-slate-200 flex items-center justify-center">
+        
+      </div>
+    </>
+  );
+};
+
+export default App;
